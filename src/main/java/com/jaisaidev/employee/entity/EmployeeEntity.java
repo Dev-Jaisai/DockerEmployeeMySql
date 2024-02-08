a43baa048a747052cefe353f8fd4bdd130b22ac2
Your Name <you@example.com>
2024-02-08 14:15:25 +0530

    Added docker file and docker compose to connect with mysql db in a container

--- src/main/java/com/jaisaidev/employee/entity/EmployeeEntity.java
+++ src/main/java/com/jaisaidev/employee/entity/EmployeeEntity.java
@@ -1,11 +1,14 @@
 package com.jaisaidev.employee.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.*;
 
 @Entity
-@Data
+
 @Table(name = "employees")
 public class EmployeeEntity {
 
@@ -15,4 +18,47 @@ public class EmployeeEntity {
     private String firstName;
     private String lastName;
     private String emailId;
+
+    public EmployeeEntity(long id, String firstName, String lastName, String emailId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public EmployeeEntity() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
 }
